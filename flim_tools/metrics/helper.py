f772dd193e73b047f3476329fbb57cd9a6a4ae66
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2021-10-04 09:48:20 -0500

    replaced assert with raising ValueError

--- flim_tools/metrics/helper.py
+++ flim_tools/metrics/helper.py
@@ -9,11 +9,15 @@ def _validate_array_and_make_bool(mask):
     # convert to bool and remove 1D axis
     mask = mask.astype(bool).squeeze()
 
+    
     # check that it's 2d
     n_dims = len(mask.shape)
-    assert n_dims == 2, f"Shape Error: mask should be 2D, {n_dims} dimensions found"
 
-    # check that mask has data
-    assert np.sum(mask) != 0, "Error: no data found in mask"
+    if n_dims != 2:
+        raise ValueError(f"Shape Error: mask should be 2D, {n_dims} dimensions found")
+  
+    if np.sum(mask) == 0:
+        # check that mask has data
+        raise ValueError("Error: no data found in mask")
 
     return mask
