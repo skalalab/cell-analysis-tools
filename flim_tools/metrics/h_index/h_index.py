6a95f8ebb14b420d3740fd18af60d9e09e1412ec
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2022-04-29 14:21:17 -0500

    working on h_index calculation

--- flim_tools/metrics/h_index/h_index.py
+++ flim_tools/metrics/h_index/h_index.py
@@ -1,17 +1,17 @@
 import numpy as np
-from sklearn.datasets import make_gaussian_quantiles
 
 from scipy.stats import norm
 import matplotlib.pylab as plt
 import matplotlib as mpl
 mpl.rcParams["figure.dpi"] = 300
 
+from sklearn.mixture import GaussianMixture
+
 #%%
 def h_index(list_distributions):
     '''
     Computes the h-index given a mixture of ditributions
-    
-    R = 
+    R = number of distributions
 
     Parameters
     ----------
@@ -24,7 +24,7 @@ def h_index(list_distributions):
         calculated H-index
 
     '''
-    median_overall = np.median(list_distributions)
+    median_overall = np.median(np.concatenate(list_distributions))
     
     # compute running sum
     running_sum = 0
@@ -35,6 +35,7 @@ def h_index(list_distributions):
         
         running_sum += d * p * np.log(p)
     return -running_sum
+
 #%%
     
 if __name__ == "__main__":
@@ -48,7 +49,7 @@ if __name__ == "__main__":
         (20,4),
         (30,6),
         (50,20),
-        # (50,10),
+        (50,10),
         ]
     
     dist_all = np.zeros_like(x)
@@ -81,13 +82,42 @@ if __name__ == "__main__":
     ## plot one two three distributions
     plt.title("Distributions")
     
-    for num_dist in np.arange(start=1, stop=4): #slide in dist 
+    for num_dist in np.arange(start=1, stop=2): #slide in dist 
         pass
         list_subdists = list_distributions[:num_dist]
         
         # compute h_index
         list_subdists_values = [dist.rvs(len(x)) for dist in list_subdists]
-        h_index_value = h_index(list_subdists_values)
+        
+        # compute gaussiam mixture model 
+        all_data = np.array(list_subdists_values).reshape(-1,1)
+        # plt.hist(all_data, histtype="step")
+        # plt.show()
+        
+        # fit data into gaussian mixtures
+        
+        
+        # https://towardsdatascience.com/gaussian-mixture-model-clusterization-how-to-select-the-number-of-components-clusters-553bef45f6e4
+        # https://stats.stackexchange.com/questions/139163/selecting-the-number-of-mixtures-hidden-states-latent-variables
+        # https://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set
+        gauss = GaussianMixture(n_components=3)
+        labels = gauss.fit_predict(all_data)
+        
+        
+        list_fit_dist = [np.asarray(all_data[labels == label]).flatten() for label in np.unique(labels)]
+        
+        plt.title("histograms")
+        plt.hist(all_data, histtype='step')
+        plt.show()
+         
+        for list_data in list_fit_dist:
+            plt.hist(list_fit_dist, histtype='step', label=f'{np.mean(list_data):.3f}')
+        plt.title("histogram of fit data")
+        plt.legend()
+        plt.show()
+
+            
+        h_index_value = h_index(list_fit_dist)
         
         #plot pdf
         pdf_combined = [d.pdf(x) for d in list_subdists]
@@ -101,6 +131,9 @@ if __name__ == "__main__":
 #%%
 
 # compare graphs with gaussianmixture fit using the same number of distributions
-# 
+
+# Take graphs and run through gaussian mixture model 
+
+
     
     
